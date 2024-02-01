2a8638387fe8f0a7957070b674f865418a99d82a
Danila <devdaniellle@gmail.com>
2024-02-01 11:15:55 +0500

    added connection to amount of cards for numeration

--- src/pages/Builder.vue
+++ src/pages/Builder.vue
@@ -33,6 +33,11 @@ watch(
     deep: true
   }
 )
+
+const amountCards = computed(() => {
+  return cards.value.length + 1
+})
+
 </script>
 
 <template>
@@ -65,28 +70,28 @@ watch(
       <div>Add New Question</div>
       <div class="flex gap-2 mt-2">
         <button
-          @click="addCard(ShortTextCard, { sequenceNumber: num })"
+          @click="addCard(ShortTextCard, { sequenceNumber: amountCards })"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Short.svg" alt="short" />
           <div>Short</div>
         </button>
         <button
-          @click="addCard(LongTextCard, { sequenceNumber: num })"
+          @click="addCard(LongTextCard, { sequenceNumber: amountCards })"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Long.svg" alt="long" />
           <div>Long</div>
         </button>
         <div
-          @click="addCard(SingleQuestionCard, { sequenceNumber: num })"
+          @click="addCard(SingleQuestionCard, { sequenceNumber: amountCards })"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Single.svg" alt="single" />
           <div>Single</div>
         </div>
         <button
-          @click="addCard(MultiQuestionCard, { sequenceNumber: num })"
+          @click="addCard(MultiQuestionCard, { sequenceNumber: amountCards })"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Multi.svg" alt="multi" />
