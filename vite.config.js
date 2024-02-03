ade49bc650e19ac2a99eda60d357666ca4e9d4b1
Danila <devdaniellle@gmail.com>
2024-02-03 18:12:33 +0500

    vite config edit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: './Home',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
