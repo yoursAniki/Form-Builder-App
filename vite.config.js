f030cb629018de83c1fcc34a6e5fbf731b01e999
Danila <devdaniellle@gmail.com>
2024-02-03 20:41:11 +0500

    config edit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Form-Builder-App/',
+  base: '/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
