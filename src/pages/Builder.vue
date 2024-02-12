9a2356bbb705122e66c6d8f025f07d4836c874c7
Danila <devdaniellle@gmail.com>
2024-02-12 13:40:39 +0500

    fixed render bug (#16)

--- src/pages/Builder.vue
+++ src/pages/Builder.vue
@@ -10,6 +10,13 @@ import LongTextCard from '../components/LongTextCard.vue'
 import SingleQuestionCard from '../components/SingleQuestionCard.vue'
 import MultiQuestionCard from '@/components/MultiQuestionCard.vue'
 
+const components = {
+  ShortTextCard,
+  LongTextCard,
+  SingleQuestionCard,
+  MultiQuestionCard
+}
+
 const cards = ref([])
 
 const addCard = (cardName) => {
@@ -144,7 +151,7 @@ const goHomePage = () => {
       <component
         v-for="card in cards"
         :key="card.id"
-        :is="card.component"
+        :is="components[card.component]"
         :data="card"
         v-bind="card.props"
         @delete-request="deleteCard(card.id)"
@@ -158,28 +165,28 @@ const goHomePage = () => {
       <div>Add New Question</div>
       <div class="flex gap-2 mt-2">
         <button
-          @click="addCard(ShortTextCard)"
+          @click="addCard('ShortTextCard')"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Short.svg" alt="short" />
           <div>Short</div>
         </button>
         <button
-          @click="addCard(LongTextCard)"
+          @click="addCard('LongTextCard')"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Long.svg" alt="long" />
           <div>Long</div>
         </button>
         <div
-          @click="addCard(SingleQuestionCard)"
+          @click="addCard('SingleQuestionCard')"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Single.svg" alt="single" />
           <div>Single</div>
         </div>
         <button
-          @click="addCard(MultiQuestionCard)"
+          @click="addCard('MultiQuestionCard')"
           class="border-2 rounded px-3 py-2 cursor-pointer flex items-center flex-col transition active:bg-slate-200 hover:-translate-x-1 hover:-translate-y-1 select-none hover:shadow-md shadow-sm"
         >
           <img src="../components/icons/Multi.svg" alt="multi" />
