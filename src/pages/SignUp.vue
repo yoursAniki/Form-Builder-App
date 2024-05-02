0df290102c9136a78fe14c98ecc8a9366cd49646
Danila <devdaniellle@gmail.com>
2024-05-02 11:47:14 +0500

    added authentication

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue'
-import InputData from '@/components/InputData.vue'
 import { useAuthStore } from '@/store/auth'
 
 const authStore = useAuthStore()
@@ -26,26 +25,32 @@ const startSignUp = async () => {
         <div class="flex flex-col gap-1 sm:w-auto w-full">
           <div class="flex flex-col gap-1">
             <p class="lessons-text text-lg font-bold">Nickname or email</p>
-            <InputData
+            <input
               class="mb-4 bg-slate-200 py-3 px-3 sm:w-80 rounded-xl focus:outline-none text-black"
               type="email"
               v-model="email"
             />
           </div>
-          <div class="flex flex-col gap-1">
+          <div class="flex flex-col gap-1 relative">
             <div class="flex justify-between lessons-text">
               <p class="lessons-text text-lg font-bold">Password</p>
               <div class="text-sm primary-color">Forgot password?</div>
             </div>
-            <InputData
+            <input
               class="mb-4 bg-slate-200 py-3 px-3 sm:w-80 rounded-xl focus:outline-none text-black"
               type="password"
               v-model="password"
             />
+            <div v-if="authStore.error" class="absolute text-red-700 -bottom-3">
+              {{ authStore.error }}
+            </div>
           </div>
+          <span v-if="authStore.loading" class="loading loading-spinner loading-lg mx-auto"></span>
+
           <button
+            v-else
             @click="startSignUp"
-            class="select-button rounded-xl border-2 bg-slate-200 py-3 text-black text-xl font-semibold mt-3 select-none"
+            class="select-button rounded-xl border-2 bg-slate-200 py-3 text-black text-xl font-semibold mt-3 select-none hover:bg-slate-300 active:bg-slate-400"
           >
             Sign Up
           </button>
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 import axios from 'axios'
 
 const API_KEY = 'AIzaSyDI3hwsb45M1uQvIr6LarTM71hAuRTjb4c'
@@ -6,19 +7,52 @@ const API_KEY = 'AIzaSyDI3hwsb45M1uQvIr6LarTM71hAuRTjb4c'
 // `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${API_KEY}`
 
 export const useAuthStore = defineStore('auth', () => {
+  const userInfo = ref({
+    token: '',
+    email: '',
+    userId: '',
+    refreshToken: '',
+    expiresIn: ''
+  })
+
+  const error = ref('')
+  const loading = ref(false)
+
   const signUp = async (payload) => {
+    loading.value = true
+    error.value = ''
     try {
-      let response = axios.post(
+      let response = await axios.post(
         `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
         {
           ...payload,
           returnSecureToken: true
         }
       )
+      userInfo.value = {
+        token: response.data.idToken,
+        email: response.data.email,
+        userId: response.data.localId,
+        refreshToken: response.data.refreshToken,
+        expiresIn: response.data.expiresIn
+      }
       console.log(response.data)
     } catch (err) {
+      switch (err.response.data.error.message) {
+        case 'EMAIL_EXISTS':
+          error.value = 'This email is already exists'
+          break
+        case 'OPERATION_NOT_ALLOWED':
+          error.value = 'Operation not allowed'
+          break
+        default:
+          error.value = 'Error'
+          break
+      }
       console.log(err.response)
+    } finally {
+      loading.value = false
     }
   }
-  return { signUp }
+  return { signUp, userInfo, error, loading }
 })
